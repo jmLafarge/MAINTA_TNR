1d378026f35a60f82c9db4c977d05ee311673a36
A1008045 <A1008045@192.168.1.60>
2023-10-12 17:20:23 +0200

    + test Azure update

---
+++ Scripts/ZZ_TESTS/test Azure update/Script1697121261918.groovy
@@ -0,0 +1,6 @@
+
+import tnrDevOps.DevOpsManager
+
+
+//DevOpsManager.updateWorkItem(16301, "System.Title", "NE PAS TRAITER - Test TNR New Title")
+DevOpsManager.updateWorkItem(16301, "System.History", "Toujours présent")
